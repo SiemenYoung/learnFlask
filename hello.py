884e8b2a79e2bff4ff9f0cc8bc499e1d283ea55b
yangsm <18353150396@163.com>
2016-12-29 18:05:24 +0800

    errorhandler

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
-from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app=Flask(__name__)
 manager = Manager(app)
@@ -12,6 +12,11 @@ def init():
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html',name=name)
-
+@app.errorhandler(404)
+def page_not_found():
+	return render_template('404.html'),404
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 if __name__=='__main__':
 	manager.run()
