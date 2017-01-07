e625a76cc0bf593b263779e2bfd93ed94f60aec8
yangsm <18353150396@163.com>
2017-01-07 23:11:27 +0800

    stmp

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_script import Manager,Shell
 from flask_bootstrap import Bootstrap
@@ -6,16 +7,28 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate,MigrateCommand
+from flask.ext.mail import Mail,Message
+
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ysm:123456@localhost/flask'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.163.com'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <18080584113@163.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+mail = Mail(app)
+
 
 class Role(db.Model):
 	"""docstring for Role"""
@@ -48,6 +61,12 @@ class NameForm(Form):
 	name = StringField('What is your name',validators=[Required()])
 	submit = SubmitField('Submit')
 
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -58,6 +77,8 @@ def index():
 			user = User(username=form.name.data)
 			db.session.add(user)
 			session['konwn']=False
+			if app.config['FLASKY_ADMIN']:
+				send_email(app.config['FLASKY_ADMIN'],'NEW User','mail/new_user',user=user)
 		else:
 			session['konwn']=True
 		session['name'] = form.name.data
@@ -78,7 +99,9 @@ def page_not_found():
 
 @app.errorhandler(500)
 def internal_server_error(e):
-     return render_template('500.html'), 500
+    return render_template('500.html'), 500
+
+
 
 if __name__ == '__main__':
 #	#db.create_all()
